e78ac78aa70e0e526842c50b59168af54cd15a30
SreangRachana <163454558+SreangRachana@users.noreply.github.com>
2025-03-22 10:57:10 +0700

    Update Order.php

--- laravel/app/Models/Order.php
+++ laravel/app/Models/Order.php
@@ -26,8 +26,8 @@ class Order extends Model
     protected function orderDate(): Attribute
     {
         return Attribute::make(
-            set: fn ($value)=> Carbon::createFromFormat("d/m/Y H:i;s", $value)->format("Y-m-d H:i:s"),
-            get: fn ($value) => Carbon::parse($value)->format('d/m/Y H;i:s'),
+            set: fn ($value)=> Carbon::createFromFormat("d/m/Y H:i:s", $value)->format("Y-m-d H:i:s"),
+            get: fn ($value) => Carbon::parse($value)->format('d/m/Y H:i:s'),
 
         );
     }
