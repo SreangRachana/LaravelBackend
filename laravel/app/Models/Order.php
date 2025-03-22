aa94760d7c0138c2772384aedf88930615b8326f
SreangRachana <sreangrachana229@gmail.com>
2025-03-22 11:37:52 +0700

    Finished TP07

--- laravel/app/Models/Order.php
+++ laravel/app/Models/Order.php
@@ -26,6 +26,7 @@ class Order extends Model
     protected function orderDate(): Attribute
     {
         return Attribute::make(
+
             set: fn ($value) => Carbon::createFromFormat("d/m/Y H:i:s", $value)->format("Y-m-d H:i:s"),
             get: fn ($value) => Carbon::parse($value)->format('d/m/Y H:i:s'),
 
